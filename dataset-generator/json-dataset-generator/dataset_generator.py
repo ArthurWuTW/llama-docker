3194c65375782a7121d64c28b9ad0a351dda5aad
ArthurWuTW <lion811004@gmail.com>
2024-10-03 11:31:58 +0800

    add generator

--- dataset-generator/json-dataset-generator/dataset_generator.py
+++ dataset-generator/json-dataset-generator/dataset_generator.py
@@ -1,10 +1,29 @@
 import os
-import input_generator
+import output_generator
+import instruction_generator
+import json
 
+def genLlamaDataFormat(output_data, instructionlist):
+    llamaDataList = []
+    for instruction in instructionlist:
+        llamaData = {}
+        llamaData['instruction'] = instruction
+        llamaData['input'] = ""
+        llamaData['output'] = json.dumps(output_data)
+        
+        llamaDataList.append(llamaData);
 
+    return llamaDataList
 
 if __name__ == '__main__':
-    print(input_generator.genOutput())
+    llama_data = []
+    
+    output_data = output_generator.genOutput()
+    instructionList = instruction_generator.genInstructionList(output_data)
+    llama_data.extend(genLlamaDataFormat(output_data, instructionList))
+    
+    # print(output_data)
+    print(llama_data)
 
     
     
---
+++ dataset-generator/json-dataset-generator/instruction_generator.py
@@ -0,0 +1,79 @@
+import json
+
+API_NAME = 'ACCESSORIES'
+
+def genSerialNumber(jsonString):
+    data = json.loads(jsonString)
+    serialNr = data['SerialNumber'] 
+    return "SN=" + serialNr
+
+def genProject(jsonString):
+    data = json.loads(jsonString)
+    project = data['Project'] 
+    return "Project=" + Project
+
+def genLastAssemblyAttributeList(jsonString):
+    
+    instructionlist = []
+
+    data = json.loads(jsonString)
+    collection = list(filter(lambda p: p['LastAssembly'] == 'LAST_ASSEMBLY', data['componentCollection']))
+    # print(collection)
+    for assembly in collection[0]['Assemblylist']:
+        attribute = assembly['Attribute']
+        value = assembly['Value']
+
+        instructionlist.append('LastAssembly Assemblylist add Attrubute="' + attribute 
+        + '" Value="' + value + '"')
+    
+    return instructionlist
+
+def genLastAssemblyComponenetList(jsonString):
+    instructionlist = []
+    data = json.loads(jsonString)
+    collection = list(filter(lambda p: p['LastAssembly'] == 'LAST_ASSEMBLY', data['componentCollection']))
+    # print(collection)
+    for component in collection[0]['ComponentList']:
+        componentLoc = component['componentLoc']
+        componentPart = component['componentPart']
+        componentNumber = component['componentNumber']
+
+        instructionlist.append('LastAssembly ComponentList add componentLoc="' + componentLoc 
+        + '" componentPart="' + componentPart + '"' + ' componentNumber="' + componentNumber + '"')
+    
+    return instructionlist
+
+def genAsas(jsonString):
+    instructionlist = []
+    data = json.loads(jsonString)
+    collection = list(filter(lambda p: p['LastAssembly'] == 'LAST_ASSEMBLY', data['componentCollection']))
+    for assembly in collection[0]['Asas']:
+        attribute = assembly['Attribute']
+        value = assembly['Value']
+
+        instructionlist.append('LastAssembly Asas add Attrubute="' + attribute 
+        + '" Value="' + value + '"')
+    
+    return instructionlist
+
+
+def genInstructionList(data):
+    jsonString = json.dumps(data)
+    serialNumberInstruction = genSerialNumber(jsonString)
+    candidateInstructions = []
+    candidateInstructions.extend(genLastAssemblyAttributeList(jsonString))
+    candidateInstructions.extend(genLastAssemblyComponenetList(jsonString))
+    candidateInstructions.extend(genAsas(jsonString))
+    
+
+    print(serialNumberInstruction)
+    print(candidateInstructions)
+    
+    instructions = []
+    for candidate in candidateInstructions:
+        instructions.append(
+            'ApiName '+API_NAME+': '+serialNumberInstruction+', '+candidate
+        )
+    
+
+    return instructions
